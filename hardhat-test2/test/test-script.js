a793c1450fee1956ef117ff49982d294c8ad0ddb
Ananthan <ananthan.rajasekharan@gmail.com>
2022-10-31 09:17:02 +0530

    Update test-script.js

--- hardhat-test2/test/test-script.js
+++ hardhat-test2/test/test-script.js
@@ -1,24 +1,31 @@
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
 
-describe("NFTMarket", function () {
-  it("Should create Token and execute market sales", async function () {
-    const NFT = await ethers.getContractFactory("NFTMarket");
-    const nft = await NFT.deploy();
-    await nft.deployed();
+describe("NFTMarket", function (accounts) {
+    it("Should create Token and execute market sales", async function () {
+        const [owner, addr1] = await ethers.getSigners();
 
-    const [account1, account2] = await ethers.getSigners();
+        const Token = await ethers.getContractFactory("NFTMarket");
 
-    await nft.connect(account1).createToken("#000000");
-    await nft.connect(account2).createToken("#ffffff");
+        const hardhatToken = await Token.deploy();
 
-    expect(await nft.ownerOf(1)).to.equal(account1.address);
+        await hardhatToken.connect(owner).createToken("#000000");
+        await hardhatToken.connect(owner).createToken("#ffffff");
 
-    await nft.connect(account1).putNFTOnSale(1, {value: ethers.utils.parseEther("0.025") });
+        expect(await hardhatToken.ownerOf(1)).to.equal(owner.address);
 
-    await nft.connect(account2).finalizeSale(1, {value: ethers.utils.parseEther("1") });
+        console.log("Before Sale Owner: ", await hardhatToken.ownerOf(1), " is account owner", owner.address);
 
-    expect(await nft.ownerOf(1)).to.equal(account2.address);
+        const listingPrice = ethers.utils.parseEther("0.025");
+        
+        await hardhatToken.connect(owner).putNFTOnSale(1, { value: listingPrice });
+        await hardhatToken.connect(owner).putNFTOnSale(2, { value: listingPrice });
 
-  });
-});
+        const purchaseValue = ethers.utils.parseEther("1");
+
+        await hardhatToken.connect(addr1).finalizeSale(1, { value: purchaseValue });
+
+        console.log("After Sale Owner: ", await hardhatToken.ownerOf(1), " is account 1", addr1.address);
+
+        expect(await hardhatToken.ownerOf(1)).to.equal(addr1.address);
+    })
+})
